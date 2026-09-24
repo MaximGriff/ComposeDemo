56afb924c4298da83c8a8fc081da2f4266e382d5
MaximGriff <grisenkom34@gmail.com>
2026-09-24 19:24:40 +0700

    Slider

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
@@ -39,6 +41,24 @@ fun DemoText(message: String, fontSize: Float){
         text = message,
         fontSize = fontSize.sp,
         fontWeight = FontWeight.Bold,
+    )
+}
+
+@Preview
+@Composable
+fun DemoTextPreview(){
+    ComposeDemoTheme {
+        DemoText(message = "Welcome to Android", fontSize = 12f)
+    }
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit){
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = sliderPosition,
+        onValueChange = {onPositionChange}
     )
 }
 
