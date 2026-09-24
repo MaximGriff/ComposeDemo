36cfb1375208e87cfddc19b3fc4fc3546b47c025
MaximGriff <grisenkom34@gmail.com>
2026-09-24 19:06:15 +0700

    Compose

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,10 +39,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ComposeDemoTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) {innerPading ->
+            Greeting("Compose", modifier = Modifier.padding(innerPading))
+        }
     }
+
 }
