e78ac002c948417397172999fcc97edeff5262db
MaximGriff <grisenkom34@gmail.com>
2026-09-24 19:14:30 +0700

    Func for display text

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +35,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoText(message: String, fontSize: Float){
-    
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold,
+    )
 }
 
 //@Composable
